19d308e31727780ad71e2209b395f4cc587f5077
Torsten Sachse <torsten.sachse@gmail.com>
2021-12-16 16:05:50 +0100

    Add methods to remove nodes from graph and to obtain the cheapest one

--- graph.go
+++ graph.go
@@ -17,3 +17,25 @@ func (g *Graph) Has(node *Node) bool {
 func (g *Graph) Add(node *Node) {
 	(*g)[node] = graphVal
 }
+
+// Remove removes a node from the graph. If the node does not exist, this a no-op.
+func (g *Graph) Remove(node *Node) {
+	delete(*g, node)
+}
+
+// PopCheapest retrieves one of the cheapest nodes and removes it. This will return nil if the graph
+// is empty.
+func (g *Graph) PopCheapest(heuristic Heuristic) *Node {
+	found := false
+	cost := 0
+	var result *Node
+	for node := range *g {
+		estimatedCost := heuristic(node)
+		if !found || node.Cost+estimatedCost < cost {
+			found = true
+			result = node
+		}
+	}
+	g.Remove(result)
+	return result
+}
